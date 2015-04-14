222d6f74baba45641dbb7b3e1b35ee4156e5e337
Mukunthan <srimukunthan@gmail.com>
2015-04-15 00:35:51 +0530

    subresource locators

--- RestSample/src/com/mukunth/service/CompanyService.java
+++ RestSample/src/com/mukunth/service/CompanyService.java
@@ -2,8 +2,6 @@ package com.mukunth.service;
 
 import java.util.List;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,27 +9,26 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.mukunth.dao.CompanyDaoImpl;
 import com.mukunth.model.Company;
+import com.mukunth.model.Employee;
 
-@Path("/company")
+@Path("/companies")
+@Produces(MediaType.APPLICATION_JSON)
 public class CompanyService {
  
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response getList() {
 		List<Company> company = CompanyDaoImpl.getInstance().getCompany();
-		if(company == null)
-		return Response.status(404).entity("No Details availabe").build();
-		else
 		return Response.status(200).entity(company).build();
 	}
  
 	@GET
 	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response getCompany(@PathParam("id") int id) {
 		Company company = CompanyDaoImpl.getInstance().getCompanyByID(id);
 		if(company == null)
@@ -42,23 +39,20 @@ public class CompanyService {
 	
 	@DELETE
 	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response deleteCompany(@PathParam("id") int id) {
-		int n = CompanyDaoImpl.getInstance().deleteCompanyByID(id);
+	public Response deleteCompany(@PathParam("id") String id) {
+		int input;
+			input = Integer.parseInt(id);
+		int n = CompanyDaoImpl.getInstance().deleteCompanyByID(input);
 		if(n > 0) {
 			List<Company> company = CompanyDaoImpl.getInstance().getCompany();
-			if(company.size() > 0)
-				return Response.status(200).entity(company).build();
-			else
 				return Response.status(204).build();
 		}
 		else
 			return Response.status(404).entity("Company details not found").build();
-	}
+	}	
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response createCompany(Company company) {
 		int n = CompanyDaoImpl.getInstance().createCompanyByID(company);
 		if(n > 0) {
@@ -71,15 +65,21 @@ public class CompanyService {
 	
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response updateCompany(Company company) {
+	@Path("{id}")
+	public Response updateCompany(Company company, @PathParam("id") int id) {
+		company.setId(id);
 		int n = CompanyDaoImpl.getInstance().updateCompanyByID(company);
 		if(n > 0) {
 			List<Company> companies = CompanyDaoImpl.getInstance().getCompany();
-			return Response.status(204).entity(companies).build();
+			return Response.status(204).build();
 		}
 			else
 			return Response.status(404).entity("Id not valid").build();
+	}
+	
+	@Path("{id}/employee")
+	public EmployeeService employee(@PathParam("employeeId") String employeeId) {
+		return new EmployeeService();
 	}
 	
 } 
